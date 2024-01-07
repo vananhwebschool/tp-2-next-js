3ad935c2fb1d5769bc10d16723cfce9bdcb971b9
vananhwebschool <v_ta@etu-webschoolfactory.fr>
2024-01-07 21:03:39 +0100

    remove useState

--- src/app/games/scores/page.jsx
+++ src/app/games/scores/page.jsx
@@ -1,22 +1,18 @@
 'use client'
-import {useState} from "react";
-
 export default function Scores() {
-    const ticTacToe = useState(myStorage.getItem("tictactoe"));
-    const hangman = useState(myStorage.getItem("hangman"));
+    const ticTacToe = myStorage.getItem("tictactoe");
+    const hangman = myStorage.getItem("hangman");
     return (
         <div className="stats stats-vertical lg:stats-horizontal shadow">
 
             <div className="stat">
                 <div className="stat-title">Tic Tac Toe</div>
                 <div className="stat-value">{ticTacToe}</div>
-                <div className="stat-desc">{Date()}</div>
             </div>
 
             <div className="stat">
                 <div className="stat-title">Hangman</div>
                 <div className="stat-value">{hangman}</div>
-                <div className="stat-desc">{Date()}</div>
             </div>
 
         </div>
